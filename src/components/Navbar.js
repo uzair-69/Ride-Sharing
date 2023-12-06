3b27d5dfd020750a825fde26c5a7a3bb87ca9fe8
Arif Hussain <arif3337179@gmail.com>
2023-12-06 12:55:13 +0500

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div>
+            <Navbar bg="primary" variant="dark">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            </Navbar>
+        </div>
+    )
+}
