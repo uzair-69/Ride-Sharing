152429149df0515e0064bd3c2512685d8e3b2df6
Arif Hussain <arif3337179@gmail.com>
2023-12-06 15:21:40 +0500

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Homepage/Navbar';
-
+import Driver from './components/Platform/Driver/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <>
+      <Navbar />
+      <Routes>
 
-    <div className="App">
+        <Route path="/Driver" element={<Driver />} />
 
-    <Navbar />
-
-
-
-      
-    </div>
+      </Routes>
 
 
+
+
+
+
+
+
+
+    </>
   );
 }
 
