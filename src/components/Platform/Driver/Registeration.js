b20a6e667efa23f60e723bf90a2aefaa8a873376
uzair-69 <muhammaduzair.bssef21@iba-suk.edu.pk>
2023-12-09 16:59:15 +0500

    Button

---
+++ src/components/Platform/Driver/Registeration.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Registration() {
+  return (
+    // <div
+      // className="container-fluid "
+      // style={{ backgroundColor: "#39334F", color: "white" }}
+    // >
+       <div 
+         className="container d-flex justify-content-center mt-5" 
+         style={{ height: "80vh", backgroundColor: "#39334F", color: "white" }} 
+       > 
+        <h1>Register Yourself</h1>
+        <div className="row">
+          <div className="col-md-6 col-lg-12">
+            
+          </div>
+        </div>
+      {/* </div> */}
+     
+    </div>
+  );
+}
