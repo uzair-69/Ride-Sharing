36197a4313d75da685c2c1b565a940a10b149872
uzair-69 <muhammaduzair.bssef21@iba-suk.edu.pk>
2023-12-10 18:41:08 +0500

    Button

--- src/components/Platform/Driver/Home.js
+++ src/components/Platform/Driver/Home.js
@@ -13,14 +13,20 @@ export default function HomePage() {
         borderRadius: '8px', // Optional: Add rounded corners
         fontSize: '18px', // Optional: Adjust the font size
         fontWeight: 'bold', // Optional: Adjust the font weight
-        }} className=''><Link to='/DriverLogin'> Login </Link></Button>{' '}
+        marginRight: '10px'
+        }} className=''><Link to='/DriverLogin' style={{textDecoration: 'none', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', width: '200px', 
+        borderRadius: '8px', color: 'white', 
+        fontSize: '18px'}}> Login </Link></Button>{' '}
 
         
          <Button variant="outline-info" style={{  width: '200px', // Adjust the width as needed
         borderRadius: '8px', // Optional: Add rounded corners
         fontSize: '18px', // Optional: Adjust the font size
         fontWeight: 'bold', // Optional: Adjust the font weight
-       }} className=''><Link to='/DriverRegistration'>Signup</Link></Button>{' '}
+        textDecoration: 'none', fontFamily: 'Arial, sans-serif'
+       }} className=''><Link to='/DriverRegistration' style={{textDecoration: 'none', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', width: '200px', 
+       borderRadius: '8px', color: 'white',
+       fontSize: '18px'}} >Signup</Link></Button>{' '}
         {/* </div>x */}
       {/* </div> */}
     </div>
